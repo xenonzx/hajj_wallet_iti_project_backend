70f24b448bd72363b059bdbdd09da477268ce594
nadafarouk <nada.h.farouk@gmail.com>
2019-05-25 10:25:24 +0200

    update .gitignore

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import AbstractUser
 class Nationality(models.Model):
     name=models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 class Account(AbstractUser):
     GENDER = (
         ('F', 'Female'),
