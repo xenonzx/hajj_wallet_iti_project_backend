4e31626a6996db7f8c44d165d9bba1ecacd9bf24
ziadmabdelhamed <ziadmabdelhamed@gmail.com>
2019-06-12 01:49:58 +0200

    add auth to search

--- search/views.py
+++ search/views.py
@@ -5,31 +5,31 @@ from rest_framework.response import Response
 from django.http import HttpResponse, JsonResponse
 
 from django.contrib.gis.geos import Point
-from django.contrib.gis.measure import Distance
 from vendors.models import Vendor
 from vendors.api.serializers import VendorSerializer
-from django.contrib.gis.geos import Polygon
 
-
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view
 
 
-@api_view(['GET'])
-def retrieve_nearest_vendors(request, format=None):
-    if request.method == 'GET':
-        lng = float(request.data['long'])
-        lt = float(request.data['lat'])
-        radius = request.data['radius']
-        category = request.data['category']
-        point = Point(lng,lt)
-        print(point)
-        if category =='':
-            data =Vendor.objects.filter(location__distance_lt=(point, radius))
-        else:
-            data = Vendor.objects.filter(location__distance_lt=(point, radius),category_id = category)
+# @api_view(['GET'])
+class Search(APIView):
+    permission_classes = (IsAuthenticated,)
+    def get(self,request, format=None):
+        if request.method == 'GET':
+            lng = float(request.data['long'])
+            lt = float(request.data['lat'])
+            radius = request.data['radius']
+            category = int(request.data['category'])
+            point = Point(lng,lt)
+            print(point)
+            if category =='':
+                data =Vendor.objects.filter(location__distance_lt=(point, radius))
+            else:
+                data = Vendor.objects.filter(location__distance_lt=(point, radius),category_id = category)
 
 
-        serializer = VendorSerializer(data, many=True)
+            serializer = VendorSerializer(data, many=True)
 
-        return JsonResponse(serializer.data,safe=False)
+            return JsonResponse(serializer.data,safe=False)
 
