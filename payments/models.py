f1b9b376be13a8dfe1c536999d5bd8a6dac01b2b
nadafarouk <nada.h.farouk@gmail.com>
2019-06-17 20:15:07 +0200

    update payment

--- payments/models.py
+++ payments/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from accounts.models import Account
-from datetime import datetime
-
 class Transaction(models.Model):
     pilgrim=models.ForeignKey(Account,on_delete=models.SET_NULL,null=True,related_name='pilgrim_id')
     vendor=models.ForeignKey(Account,on_delete=models.SET_NULL,null=True,related_name='vendor_id')
